e4b559f78476e7c07bd13c90e4ee5f0a0c5129ce
estevaoanjo <estevaosilvino25@gmail.com>
2026-09-19 01:09:13 -0300

    Add request of file and livereload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from livereload import Server
 
 app = Flask(__name__)
 
@@ -7,6 +8,24 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route("/gerar-orcamentos", methods=["POST"])
+def gerarorcamento():
+
+    logo = request.files.get("logo")
+
+    if logo and logo.filename:
+        print("Logo recebida", logo.filename)
+    else:
+        print("Logo não recebida")
+
+    return "Orçamento recebido"
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    server = Server(app.wsgi_app)
+
+    server.watch("templates/")
+    server.watch("static/css/")
+    server.watch("static/js/")
+
+    server.serve(port=5000)
